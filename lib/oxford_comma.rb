8080a3a5f726e118701457ea520bf968c733167a
Javier <tsakasu88@gmail.com>
2021-01-24 16:25:36 -0500

    commit tree 8531e9c76dc62b15f8af578cebf1f3015fb7deda

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        return array.join()
+      elsif array.length == 2
+        return "#{array.first} and #{array.last}" 
+      elsif array.length > 2
+       three_array = array
+       three_array[-1] = "and #{three_array[-1]}"
+       three_array.join(', ')     
+      end
 end
